12ce618cfb1f4a7fd63ef7c0b6a97b873a564622
goos-e <exonterous@gmail.com>
2021-12-24 17:38:21 +0000

    edited part1

--- day3/day3.py
+++ day3/day3.py
@@ -7,16 +7,16 @@ def create_array():
 def part1(array):
     gamma = ""
     epsilon = ""
+    counts = {i: 0 for i in range(12)}
+    half = len(array)/2
 
-    for pos in range(12):
-        count_1 = 0
-        count_0 = 0
-        for e in array:
-            if e[pos] == "1":
-                count_1 += 1
-            else:
-                count_0 += 1
-        if count_1 > count_0:
+    for e in array:
+        for i, c in enumerate(e):
+            if c == "1":
+                counts[i] += 1
+
+    for e in counts:
+        if counts[e] >= half:
             gamma += "1"
             epsilon += "0"
         else:
@@ -27,7 +27,27 @@ def part1(array):
 
 
 def part2(array):
-    
+    oxy_array = array
+
+    for pos in range(12):
+        count_0 = 0
+        count_1 = 0
+
+        for _ in range(2):
+            for i, e in enumerate(oxy_array):
+
+                if _ == 0:
+                    if e[pos] == 1:
+                        count_1 += 1
+                    else:
+                        count_0 += 1
+
+                if _ == 1:
+                    x = 1 if count_1 >= count_0 else 0
+
+
+
+
 
 
 if __name__ == '__main__':
